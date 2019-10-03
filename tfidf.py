280765a914255c2732e949be8442d09b627317fd
Oliver Widder <oliver.widder@gmail.com>
2019-10-03 16:58:03 +0200

    refactor tfidf.py

--- tfidf.py
+++ tfidf.py
@@ -1,110 +1,12 @@
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
-
-import nltk
-import string
 import os
 import errno
 import re
 import sys
-import numpy as np
-import enchant
+import nltk
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-nltk.download('punkt')
-nltk.download('stopwords')
-
-
-en = enchant.Dict("en_US")
-de = enchant.Dict("de_DE")
-fr = enchant.Dict("fr_FR")
-
-unstem_dict = {}
-english_words = []
-
-def tokenize(text):
-    tokens = nltk.word_tokenize(text)
-    return tokens
-
-
-def splitCamelCase(name):
-    return re.sub('([a-z0-9])([A-Z])', r'\1 \2', name).lower()
-
-
-def removeSingleChars(string):
-    return ' '.join([w for w in string.split() if len(w) > 1])
-
-
-def convert_lower_case(data):
-    return np.char.lower(data)
-
-
-def remove_stop_words(data):
-    stop_words = stopwords.words('english')
-    words = word_tokenize(str(data))
-    new_text = ""
-    for w in words:
-        if w not in stop_words and len(w) > 1:
-            new_text = new_text + " " + w
-    return new_text
-
-
-def add_to_unstem_dict(word, stemmed_word):
-    if stemmed_word in unstem_dict:
-        if len(word) < len(unstem_dict[stemmed_word]):
-            unstem_dict[stemmed_word] = word
-    else:
-        unstem_dict[stemmed_word] = word
-
-
-def unstem(word):
-    return unstem_dict[word]
-
-
-def stemming(data):
-    stemmer = PorterStemmer()
-
-    tokens = word_tokenize(str(data))
-    new_text = ""
-    for w in tokens:
-        stemmed_word = stemmer.stem(w)
-        add_to_unstem_dict(w, stemmed_word)
-        new_text = new_text + " " + stemmer.stem(w)
-
-    return new_text
-
-
-def filter_non_english_words(data):
-    tokens = word_tokenize(str(data))
-    return " ".join(list(filter(lambda w: unstem(w) in english_words, tokens)))
-
-
-def is_en_de_fr(word):
-    return en.check(word) or de.check(word) or fr.check(word)
-
-
-def filter_non_en_de_fr_words(data):
-    tokens = word_tokenize(str(data))
-    return " ".join(list(filter(lambda w: is_en_de_fr(unstem(w)), tokens)))
-
-
-def fileString(file_path):
-    try:
-        shakes = open(file_path, 'r')
-        text = shakes.read()
-        no_punctuation = text.translate(string.punctuation)
-        only_a_to_z = re.sub('[^A-Za-z ]+', ' ', no_punctuation)
-        camel_case_split = splitCamelCase(only_a_to_z)
-        camel_case_split_no_single_chars = removeSingleChars(camel_case_split)
-        camel_case_split_no_single_chars_no_stop_words = remove_stop_words(camel_case_split_no_single_chars)
-        camel_case_split_no_single_chars_no_stop_words_stemmed = stemming(camel_case_split_no_single_chars_no_stop_words)
-        camel_case_split_no_single_chars_no_stop_words_stemmed_only_english = filter_non_en_de_fr_words(camel_case_split_no_single_chars_no_stop_words_stemmed)
-        return camel_case_split_no_single_chars_no_stop_words_stemmed_only_english
-    except:
-        return ""
-
+from .words_of_file import words_of_file, unstem
 
 def isNoDotFile(file_path):
     return len(list(filter(lambda part: part.startswith("."), file_path.split("/")))) == 0
@@ -129,10 +31,15 @@ def tokenizePath(path):
         for file in files:
             file_path = subdir + os.path.sep + file
             if isIncluded(file_path):
-                wordsOfFile = fileString(file_path)
+                wordsOfFile = words_of_file(file_path)
                 if len(wordsOfFile) > 0:
                     token_dict[file] = wordsOfFile
     return token_dict
+
+
+def tokenize(text):
+    tokens = nltk.word_tokenize(text)
+    return tokens
 
 
 def fit(corpusPath):
@@ -154,11 +61,6 @@ def openFileForWritingWithPathCreation(file_path):
     return open(file_path, 'w')
 
 
-def readEnglishWords():
-    global english_words
-    english_words = [line.rstrip('\n') for line in open("./english-words/corncob_lowercase.txt").readlines()]
-
-
 def findFeatures(corpusPath, rootPath):
     print("---- do the fitting ----\n")
     tfidf = fit(corpusPath=corpusPath)
@@ -169,7 +71,7 @@ def findFeatures(corpusPath, rootPath):
             if isIncluded(file_path):
                 out_file_path = "out/" + relPathFromAbsPath(rootPath, file_path) + ".tfidf.csv"
                 print("--> " + out_file_path)
-                file_str = fileString(file_path)
+                file_str = words_of_file(file_path)
                 file_response = tfidf.transform([file_str])
                 feature_names = tfidf.get_feature_names()
 
@@ -187,7 +89,5 @@ def findFeatures(corpusPath, rootPath):
 
 corpusPath = sys.argv[1]
 rootPath = (sys.argv[2] if len(sys.argv) > 2 else corpusPath)
-
-readEnglishWords()
 
 findFeatures(corpusPath=corpusPath, rootPath=rootPath)
