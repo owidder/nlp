dd760da907524fc9e75544adf1940f9d7f506a15
Oliver Widder <oliver.widder@gmail.com>
2019-10-03 19:04:08 +0200

    isIncluded -> is_included

--- python/util/tokenize.py
+++ python/util/tokenize.py
@@ -11,7 +11,7 @@ def hasNoExcludedExtension(file_path):
   return extension.lower() not in ["jpg", "jpeg", "png", "bmp", "csv"]
 
 
-def isIncluded(file_path):
+def is_included(file_path):
     return isNoDotFile(file_path) and hasNoExcludedExtension(file_path)
 
 
@@ -20,7 +20,7 @@ def tokenize_path(path):
     for subdir, dirs, files in os.walk(path):
         for file in files:
             file_path = subdir + os.path.sep + file
-            if isIncluded(file_path):
+            if is_included(file_path):
                 wordsOfFile = words_of_file(file_path)
                 if len(wordsOfFile) > 0:
                     token_dict[file] = wordsOfFile
