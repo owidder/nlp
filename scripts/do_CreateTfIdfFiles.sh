c6240ea1551004c43d12744f9723886658fc4f07
Oliver Widder <oliver.widder@gmail.com>
2019-10-15 19:00:16 +0200

    fix script

--- scripts/do_CreateTfIdfFiles.sh
+++ scripts/do_CreateTfIdfFiles.sh
@@ -1,1 +1,1 @@
-python3 /root/git/nlpDocs/python/read_or_create_word_unstem_dicts.py --docpath /root/git/erpnext --dictpath /root/git/nlpDocs/dict --name erpnext --outpath /root/git/nlpDocs/out
+python3 /root/git/nlpDocs/python/tfidf.py --docpath /root/git/erpnext --dictpath /root/git/nlpDocs/dict --name erpnext --outpath /root/git/nlpDocs/out
