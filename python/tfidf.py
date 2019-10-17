0aad23fb04e107296b426019cdad09f2e462310f
Oliver Widder <oliver.widder@gmail.com>
2019-10-17 19:09:24 +0200

    use get_args in tfidf

--- python/tfidf.py
+++ python/tfidf.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 from words.words_of_file import read_or_create_word_unstem_dict, is_included
 from util.util import rel_path_from_abs_path, open_file_for_writing_with_path_creation
+from util.get_args import get_args
 
 
 def tokenize(text):
@@ -56,14 +57,7 @@ def find_features(doc_path, word_dict_path, out_path,  name):
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--docpath', required=True, action='store', help='Path to the documents')
-    parser.add_argument('--dictpath', required=True, action='store', help='Path to the dictionarty. Has to be name word_dict.<name>.pickle. If it does not exist, it will be created')
-    parser.add_argument('--outpath', required=True, action='store', help='Path to the output folder')
-    parser.add_argument('--name', required=True, action='store', help='Name of the dictionary (see paramter --dictpath)')
-
-    args = parser.parse_args()
-
+    args = get_args(doc_path_required=True, dict_path_required=True, out_path_required=True, name_required=True)
     find_features(doc_path=args.docpath, word_dict_path=args.dictpath, out_path=args.outpath, name=args.name)
 
 
