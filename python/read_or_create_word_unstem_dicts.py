58c403494fe7646eb32bd3f09ea401b771b44e74
Oliver Widder <oliver.widder@gmail.com>
2019-10-17 19:07:22 +0200

    use get_args

--- python/read_or_create_word_unstem_dicts.py
+++ python/read_or_create_word_unstem_dicts.py
@@ -1,17 +1,8 @@
-import sys
-import argparse
-
+from util.get_args import get_args
 from words.words_of_file import read_or_create_word_unstem_dict
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--docpath', required=True, action='store', help='Path to the documents')
-    parser.add_argument('--dictpath', required=True, action='store', help='Path to the dictionarty. Has to be name word_dict.<name>.pickle. If it does not exist, it will be created')
-    parser.add_argument('--name', required=True, action='store', help='Name of the dictionary (see paramter --dictpath)')
-    args = parser.parse_args()
-
+    args = get_args(doc_path_required=True, dict_path_required=True, name_required=True)
     word_unstem_dicts = read_or_create_word_unstem_dict(doc_path=args.docpath, dict_path=args.dictpath, name=args.name)
-    print(word_unstem_dicts.word_dict.keys())
-    print(word_unstem_dicts.unstem_dict.keys())
 
 
