19b87ed330a9afacc72ef782d4323cc821977fbb
Oliver Widder <oliver.widder@gmail.com>
2019-10-20 20:38:25 +0200

    createIndex

--- python/createIndex.py
+++ python/createIndex.py
@@ -1,17 +1,25 @@
-import logging
-import os
 from gensim import corpora, models, similarities
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+from get_args import get_args
 
-dictionary = corpora.Dictionary.load('./data/corpus.dict')
-corpus = corpora.MmCorpus('./data/corpus.mm')
-tfidf = models.TfidfModel(corpus)
+def create_index(dict_path, name):
+    dictionary = corpora.Dictionary.load('./data/corpus.dict')
+    corpus = corpora.MmCorpus('./data/corpus.mm')
+    tfidf = models.TfidfModel(corpus)
 
-corpus_tfidf = tfidf[corpus]
-lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=100)
-lsi.save('./data/corpus.lsi')
-corpus_lsi = lsi[corpus_tfidf]
+    corpus_tfidf = tfidf[corpus]
+    lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=100)
+    lsi.save('./data/corpus.lsi')
+    corpus_lsi = lsi[corpus_tfidf]
 
-index = similarities.MatrixSimilarity(lsi[corpus])
-index.save('./data/corpus.100.index')
+    index = similarities.MatrixSimilarity(lsi[corpus])
+    index.save('./data/corpus.100.index')
+
+
+def main():
+    args = get_args(dict_path_required=True, name_required=True)
+    create_index(dict_path=args.dictpath, name=args.name)
+
+
+if __name__ == "__main__":
+    main()
