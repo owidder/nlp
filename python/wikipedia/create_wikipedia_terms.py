b45d6bc46495e8bb6305d9d7d17cf3122a28490b
Oliver Widder <oliver.widder@gmail.com>
2019-11-11 20:54:16 +0100

    use Set[str] to create wikipedia terms

--- python/wikipedia/create_wikipedia_terms.py
+++ python/wikipedia/create_wikipedia_terms.py
@@ -1,6 +1,8 @@
 import re
 import argparse
 
+from typing import Set
+
 
 def remove_hex_chars(words: str) -> str:
     return re.sub('%[0-9A-Fa-f][0-9A-Fa-f]', ' ', words)
@@ -20,19 +22,29 @@ def remove_single_chars(words):
     return ' '.join([w for w in words.split() if len(w) > 1])
 
 
-def create_wikipedia_terms(titles_path, out_path):
-    open(out_path, 'w').close()
-    with open(out_path, 'a') as out_file:
-        with open(titles_path) as titles_file:
+def create_wikipedia_terms(titles_path) -> Set[str]:
+    terms = set()
+
+    with open(titles_path) as titles_file:
+        words = titles_file.readline()
+        while words:
+            words = remove_hex_chars(words)
+            words = only_chars_and_numbers(words)
+            words = remove_numbers(words)
+            words = remove_single_chars(words)
+            for word in words.split():
+                terms.add(word)
             words = titles_file.readline()
-            while words:
-                words = remove_hex_chars(words)
-                words = only_chars_and_numbers(words)
-                words = remove_numbers(words)
-                words = remove_single_chars(words)
-                for word in words.split():
-                    out_file.write(f"{word}\n")
-                words = titles_file.readline()
+
+    return terms
+
+
+def write_terms(out_path: str, terms: Set[str]):
+    terms_list = list(terms)
+    terms_list.sort()
+    with open(out_path, 'w') as out_file:
+        for term in terms_list:
+            out_file.write(f"{term}\n")
 
 
 if __name__ == '__main__':
@@ -40,4 +52,5 @@ if __name__ == '__main__':
     parser.add_argument('--titles', required=True, action='store', help='Path to the wikipedia titles file')
     parser.add_argument('--out', required=True, action='store', help='Path to the output file')
     args = parser.parse_args()
-    create_wikipedia_terms(titles_path=args.titles, out_path=args.out)
+    terms = create_wikipedia_terms(titles_path=args.titles)
+    write_terms(out_path=args.out, terms=terms)
