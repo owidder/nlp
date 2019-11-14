e1da0cfe7809c403177a7e9c4da94ae2794542fa
Oliver Widder <oliver.widder@gmail.com>
2019-11-14 17:43:03 +0000

    fix script

--- scripts/do_createWikipediaTerms.sh
+++ scripts/do_createWikipediaTerms.sh
@@ -1,1 +1,1 @@
-python3 /root/git/nlpDocs/python/wikipedia/create_wikipedia_terms.py --titles /root/git/dataForNlp/wikipedia/title.txt --out /root/git/nlpDocs/dict
+python3 /root/git/nlpDocs/python/wikipedia/create_wikipedia_terms.py --titles /root/git/dataForNlp/wikipedia/titles.txt --out /root/git/nlpDocs/dict
