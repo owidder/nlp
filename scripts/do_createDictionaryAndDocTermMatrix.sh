bf9e7909b1c5f0c8dcba0f14c4a6f1a326503f2c
Oliver Widder <oliver.widder@gmail.com>
2019-11-24 12:52:49 +0100

    fix shell scripts

--- scripts/do_createDictionaryAndDocTermMatrix.sh
+++ scripts/do_createDictionaryAndDocTermMatrix.sh
@@ -1,1 +1,1 @@
-python3 /root/git/nlpDocs/python/read_or_create_word_dict.py --docpath /root/git/erpnext --dictpath /root/git/nlpDocs/dict --name erpnext --filterlevel HARD --term_infos_path /root/git/nlpClient/server --term_infos_name small
+python3 /root/git/nlpDocs/python/create_dictionary_and_doc_term_matrix.py --docpath /root/git/erpnext --dictpath /root/git/nlpDocs/dict --name erpnext --filterlevel HARD --term_infos_path /root/git/nlpClient/server --term_infos_name small
---
+++ scripts/do_createDictionaryAndDocTermMatrix_base.sh
@@ -0,0 +1,1 @@
+python3 /root/git/nlpDocs/python/create_dictionary_and_doc_term_matrix.py --docpath /root/git/erpnext --dictpath /root/git/nlpDocs/dict --name erpnext
