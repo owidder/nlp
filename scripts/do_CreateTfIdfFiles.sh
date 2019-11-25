d90cedd25cd4b6b224d5731f055125f4a1c491a1
Oliver Widder <oliver.widder@gmail.com>
2019-11-25 07:57:03 +0100

    tfidf start script

--- scripts/do_CreateTfIdfFiles.sh
+++ scripts/do_CreateTfIdfFiles.sh
@@ -1,1 +1,1 @@
-python3 /root/git/nlpDocs/python/tfidf.py --docpath /root/git/erpnext --dictpath /root/git/nlpDocs/dict --name erpnext --outpath /root/git/nlpDocs/out
+python3 /root/git/nlpDocs/python/tfidf.py --docpath /root/git/erpnext --dictpath /root/git/nlpDocs/dict --name erpnext --outpath /root/git/nlpDocs/out/erpnext
