bc0b372328937ea78cf3ac581b0d0cb5172e91ac
Oliver Widder <oliver.widder@gmail.com>
2021-08-17 10:38:59 +0200

    antlrProxy.py

--- python/antlr/antlrProxy.py
+++ python/antlr/antlrProxy.py
@@ -1,5 +1,7 @@
 from py4j.java_gateway import JavaGateway
 import subprocess
+import time
+import sys
 
 from python.get_args import get_str_env_var, PATH_TO_JAR
 
@@ -18,12 +20,21 @@ class AntlrProxy:
         self.sub = subprocess.Popen(["java", "-jar",  get_str_env_var(PATH_TO_JAR, "")])
         print("------------- JVM restarted -----------------")
 
+    def _startListener(self, file_path: str, ctr: int) -> str:
+        try:
+            words = self.gateway.entry_point.startListener(file_path)
+            print(words)
+            return words
+        except:
+            print(sys.exc_info()[0])
+            if ctr > 20:
+                sys.exit(-1)
+            time.sleep(.5)
+            return self._startListener(file_path, ctr+1)
+
     def startListener(self, file_path) -> str:
         self.counter += 1
-        if self.counter > 1000:
+        if self.counter > 10:
             self.restartJvm()
             self.counter = 0
-
-        words = self.gateway.entry_point.startListener(file_path)
-        print(words)
-        return words
+        return self._startListener(file_path, 0)
