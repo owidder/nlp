4a3c5971a2aba067d2b83f86a859a7693cf28baf
Oliver Widder <oliver.widder@gmail.com>
2021-10-14 19:21:17 +0200

    new folder structure on digital ocean

--- start-vector-do.sh
+++ start-vector-do.sh
@@ -12,8 +12,9 @@ export OUT_SUB_FOLDER=antlr
 export MIN_TFIDF=0.18
 export PATH_TO_JAR=/root/dev/antlr/build/libs/antlr-1.0-SNAPSHOT.jar
 export USE_ANTLR=True
-python3 -m python.create_vectors --outpath /root/dev/nlp/dict/erpnext --docpath /root/dev/erpnext
-python3 -m python.create_vectors --outpath /root/dev/nlp/dict/adempiere --docpath /root/dev/adempiere
-python3 -m python.create_vectors --outpath /root/dev/nlp/dict/axelor-open-suite --docpath /root/dev/axelor-open-suite
-python3 -m python.create_vectors --outpath /root/dev/nlp/dict/dolibarr --docpath /root/dev/dolibarr
-python3 -m python.create_vectors --outpath /root/dev/nlp/dict/metafresh --docpath /root/dev/metafresh
+#python3 -m python.create_vectors --outpath /root/dev/nlp/dict/erpnext --docpath /root/dev/erp_doc/erpnext
+#python3 -m python.create_vectors --outpath /root/dev/nlp/dict/adempiere --docpath /root/dev/erp_doc/adempiere
+#python3 -m python.create_vectors --outpath /root/dev/nlp/dict/axelor-open-suite --docpath /root/dev/erp_doc/axelor-open-suite
+#python3 -m python.create_vectors --outpath /root/dev/nlp/dict/dolibarr --docpath /root/dev/erp_doc/dolibarr
+#python3 -m python.create_vectors --outpath /root/dev/nlp/dict/metafresh --docpath /root/dev/erp_doc/metafresh
+python3 -m python.create_vectors --outpath /root/dev/nlp/dict/_all --docpath /root/dev/erp_doc
