01b0ba3b32baca55a57405dc30f6e5059a2401eb
Oliver Widder <oliver.widder@gmail.com>
2022-01-15 15:36:48 +0100

    start-aggregate-do.sh

--- start-aggregate-do.sh
+++ start-aggregate-do.sh
@@ -1,8 +1,4 @@
 export PYTHONUNBUFFERED=1
 export PYTHONPATH=/root/dev/nlp/python
 export OUT_SUB_FOLDER=antlr
-python3 -m python.aggregate --outpath /root/dev/nlp/dict/erpnext
-python3 -m python.aggregate --outpath /root/dev/nlp/dict/adempiere
-python3 -m python.aggregate --outpath /root/dev/nlp/dict/axelor-open-suite
-python3 -m python.aggregate --outpath /root/dev/nlp/dict/dolibarr
-python3 -m python.aggregate --outpath /root/dev/nlp/dict/metafresh
+python3 -m python.aggregate --outpath /root/dev/nlp/dict/_all
