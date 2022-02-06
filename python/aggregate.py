d94f62e0e64cc50a9d82e7381ee6d7f8323d61f3
Oliver Widder <oliver.widder@gmail.com>
2022-02-06 11:32:34 +0100

    avg (1)

--- python/aggregate.py
+++ python/aggregate.py
@@ -5,9 +5,12 @@ from get_args import get_args, get_str_env_var, OUT_SUB_FOLDER
 
 FOLDER_SUM_VALUES_FILE_NAME = '_sum.csv'
 FOLDER_MAX_VALUES_FILE_NAME = '_max.csv'
+FOLDER_AVG_VALUES_FILE_NAME = '_avg.csv'
 
 
-def aggregate_values_in_file(sum_values_file_path: str, current_sum_values: dict, max_values_file_path: str, current_max_values: dict):
+def aggregate_values_in_file(sum_values_file_path: str, current_sum_values: dict,
+                             max_values_file_path: str, current_max_values: dict,
+                             current_count_values: dict):
     with open(sum_values_file_path, 'r') as sum_f:
         for sum_line in sum_f:
             sum_kv = sum_line.split("\t")
@@ -15,8 +18,10 @@ def aggregate_values_in_file(sum_values_file_path: str, current_sum_values: dict
             sum_v = float(sum_kv[1])
             if sum_k in current_sum_values:
                 current_sum_values[sum_k] += sum_v
+                current_count_values[sum_k] += 1
             else:
                 current_sum_values[sum_k] = sum_v
+                current_count_values[sum_k] = 1
 
     with open(max_values_file_path, 'r') as max_f:
         for max_line in max_f:
@@ -29,10 +34,11 @@ def aggregate_values_in_file(sum_values_file_path: str, current_sum_values: dict
                 current_max_values[max_k] = max_v
 
 
-def aggregate_values_in_subfolder(subdir_path, current_sum_values, current_max_values):
+def aggregate_values_in_subfolder(subdir_path: str, current_sum_values: dict, current_max_values: dict):
     print(f"dir: {subdir_path}")
     csv_sum_values_file = f"{subdir_path}/{FOLDER_SUM_VALUES_FILE_NAME}"
     csv_max_values_file = f"{subdir_path}/{FOLDER_MAX_VALUES_FILE_NAME}"
+    csv_avg_values_file = f"{subdir_path}/{FOLDER_AVG_VALUES_FILE_NAME}"
     if not os.path.isfile(csv_sum_values_file) or not os.path.isfile(csv_max_values_file):
         aggregate_folder(subdir_path)
 
@@ -42,14 +48,17 @@ def aggregate_values_in_subfolder(subdir_path, current_sum_values, current_max_v
 def aggregate_folder(folder_path):
     sum_values = {}
     max_values = {}
+    count_values = {}
 
     for f in os.listdir(folder_path):
         full_path = f"{folder_path}/{f}"
         if(os.path.isfile(full_path)):
             if(f.endswith('tfidf.csv')):
-                aggregate_values_in_file(full_path, sum_values, full_path, max_values)
+                aggregate_values_in_file(sum_values_file_path=full_path, current_sum_values=sum_values,
+                                         max_values_file_path=full_path, current_max_values=max_values,
+                                         current_count_values=count_values)
         else:
-            aggregate_values_in_subfolder(full_path, sum_values, max_values)
+            aggregate_values_in_subfolder(subdir_path=full_path, current_sum_values=sum_values, current_max_values=max_values)
 
     with open(f"{folder_path}/{FOLDER_SUM_VALUES_FILE_NAME}", 'w') as out_file:
         sum_keys_sorted_after_values = sorted(sum_values, key=sum_values.__getitem__, reverse=True)
