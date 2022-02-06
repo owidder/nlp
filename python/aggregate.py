f0a968756b4caaaaa0cddc7e0bbf24e56aee691e
Oliver Widder <oliver.widder@gmail.com>
2022-02-06 14:08:14 +0100

    avg refactored

--- python/aggregate.py
+++ python/aggregate.py
@@ -3,91 +3,59 @@ import os
 from get_args import get_args, get_str_env_var, OUT_SUB_FOLDER
 
 
-FOLDER_SUM_VALUES_FILE_NAME = '_sum.csv'
-FOLDER_MAX_VALUES_FILE_NAME = '_max.csv'
-FOLDER_AVG_VALUES_FILE_NAME = '_avg.csv'
-FOLDER_COUNT_VALUES_FILE_NAME = '_count.csv'
+VALUES_FILE_NAME = '_.csv'
+
+SUM_INDEX = 0
+MAX_INDEX = 1
+COUNT_INDEX = 2
 
 
-def aggregate_values_in_file(sum_values_file_path: str, current_sum_values: dict,
-                             max_values_file_path: str, current_max_values: dict,
-                             count_values_file_path: str, current_count_values: dict):
-    with open(sum_values_file_path, 'r') as sum_f:
-        for sum_line in sum_f:
-            sum_kv = sum_line.split("\t")
-            sum_k = sum_kv[0]
-            sum_v = float(sum_kv[1])
-            if sum_k in current_sum_values:
-                current_sum_values[sum_k] += sum_v
+def aggregate_values_in_file(file_path: str, current_values):
+    with open(file_path, 'r') as f:
+        for line in f:
+            parts = line.split("\t")
+            k = parts[0]
+            if len(parts) > 2:
+                sum_v = float(parts[SUM_INDEX+1])
+                max_v = float(parts[MAX_INDEX+1])
             else:
-                current_sum_values[sum_k] = sum_v
+                sum_v = float(parts[1])
+                max_v = float(parts[1])
 
-    with open(count_values_file_path, 'r') as count_f:
-        for count_line in count_f:
-            count_k = count_line.split("\t")[0]
-            if count_k in current_count_values:
-                current_count_values[count_k] += 1
+            if k in current_values:
+                current_values[k][SUM_INDEX] += sum_v
+                current_values[k][COUNT_INDEX] += 1
+                current_values[k][MAX_INDEX] = max_v if current_values[k][MAX_INDEX] < max_v else current_values[k][MAX_INDEX]
             else:
-                current_count_values[count_k] = 1
-
-    with open(max_values_file_path, 'r') as max_f:
-        for max_line in max_f:
-            max_kv = max_line.split("\t")
-            max_k = max_kv[0]
-            max_v = float(max_kv[1])
-            if max_k in current_max_values:
-                current_max_values[max_k] = max_v if current_max_values[max_k] < max_v else current_max_values[max_k]
-            else:
-                current_max_values[max_k] = max_v
+                current_values[k] = [None, None, None]
+                current_values[k][SUM_INDEX] = sum_v
+                current_values[k][COUNT_INDEX] = 1
+                current_values[k][MAX_INDEX] = max_v
 
 
-def aggregate_values_in_subfolder(subdir_path: str, current_sum_values: dict, current_max_values: dict, current_count_values: dict):
+def aggregate_values_in_subfolder(subdir_path: str, current_values: dict):
     print(f"dir: {subdir_path}")
-    csv_sum_values_file = f"{subdir_path}/{FOLDER_SUM_VALUES_FILE_NAME}"
-    csv_max_values_file = f"{subdir_path}/{FOLDER_MAX_VALUES_FILE_NAME}"
-    csv_count_values_file = f"{subdir_path}/{FOLDER_COUNT_VALUES_FILE_NAME}"
-    if not os.path.isfile(csv_sum_values_file) or not os.path.isfile(csv_max_values_file):
+    values_file = f"{subdir_path}/{VALUES_FILE_NAME}"
+    if not os.path.isfile(values_file):
         aggregate_folder(subdir_path)
 
-    aggregate_values_in_file(sum_values_file_path=csv_sum_values_file, current_sum_values=current_sum_values,
-                             max_values_file_path=csv_max_values_file, current_max_values=current_max_values,
-                             count_values_file_path=csv_count_values_file, current_count_values=current_count_values)
+    aggregate_values_in_file(file_path=values_file, current_values=current_values)
 
 
 def aggregate_folder(folder_path):
-    sum_values = {}
-    max_values = {}
-    count_values = {}
+    values = {}
 
     for f in os.listdir(folder_path):
         full_path = f"{folder_path}/{f}"
         if(os.path.isfile(full_path)):
             if(f.endswith('tfidf.csv')):
-                aggregate_values_in_file(sum_values_file_path=full_path, current_sum_values=sum_values,
-                                         max_values_file_path=full_path, current_max_values=max_values,
-                                         count_values_file_path=full_path, current_count_values=count_values)
+                aggregate_values_in_file(file_path=full_path, current_values=values)
         else:
-            aggregate_values_in_subfolder(subdir_path=full_path, current_sum_values=sum_values, current_max_values=max_values, current_count_values=count_values)
+            aggregate_values_in_subfolder(subdir_path=full_path, current_values=values)
 
-    with open(f"{folder_path}/{FOLDER_SUM_VALUES_FILE_NAME}", 'w') as out_file:
-        sum_keys_sorted_after_values = sorted(sum_values, key=sum_values.__getitem__, reverse=True)
-        for k in sum_keys_sorted_after_values:
-            print(f"{k}\t{str(sum_values[k])}", file=out_file)
-
-    with open(f"{folder_path}/{FOLDER_MAX_VALUES_FILE_NAME}", 'w') as out_file:
-        max_keys_sorted_after_values = sorted(max_values, key=max_values.__getitem__, reverse=True)
-        for k in max_keys_sorted_after_values:
-            print(f"{k}\t{str(max_values[k])}", file=out_file)
-
-    with open(f"{folder_path}/{FOLDER_COUNT_VALUES_FILE_NAME}", 'w') as out_file:
-        count_keys_sorted_after_values = sorted(count_values, key=count_values.__getitem__, reverse=True)
-        for k in count_keys_sorted_after_values:
-            print(f"{k}\t{str(count_values[k])}", file=out_file)
-
-    with open(f"{folder_path}/{FOLDER_AVG_VALUES_FILE_NAME}", 'w') as out_file:
-        count_keys_sorted_after_values = sorted(count_values, key=count_values.__getitem__, reverse=True)
-        for k in count_keys_sorted_after_values:
-            print(f"{k}\t{str(round(sum_values[k] /  count_values[k], 2))}", file=out_file)
+    with open(f"{folder_path}/{VALUES_FILE_NAME}", 'w') as out_file:
+        for k, v in values.items():
+            print(f"{k}\t{str(v[SUM_INDEX])}\t{str(v[MAX_INDEX])}\t{str(round(v[SUM_INDEX]/v[COUNT_INDEX], 2))}", file=out_file)
 
 
 def main():
