e17d9189b1f0a53e3ad7b0e802f1515a9128b097
Oliver Widder <oliver.widder@gmail.com>
2022-02-23 13:32:38 +0100

    rename (11)

--- python/antlr/extract_essential_phrases_from_java.py
+++ python/antlr/extract_essential_phrases_from_java.py
@@ -9,28 +9,28 @@ from python.antlr.java.JavaParserListener import JavaParserListener
 
 
 class JavaListener(JavaParserListener):
-    def __init__(self, essential_words: []):
-        self.essential_words = essential_words
+    def __init__(self, essential_phrases: []):
+        self.essential_phrases = essential_phrases
 
     def enterClassDeclaration(self, ctx:JavaParser.ClassDeclarationContext):
-        self.essential_words.append(ctx.getChild(1).getText())
+        self.essential_phrases.append(ctx.getChild(1).getText())
 
     def enterMethodDeclaration(self, ctx:JavaParser.MethodDeclarationContext):
-        self.essential_words.append(ctx.getChild(1).getText())
+        self.essential_phrases.append(ctx.getChild(1).getText())
 
     def enterInterfaceDeclaration(self, ctx:JavaParser.InterfaceDeclarationContext):
-        self.essential_words.append(ctx.getChild(1).getText())
+        self.essential_phrases.append(ctx.getChild(1).getText())
 
 
 class StringJavaLexer(JavaLexer):
-    def __init__(self, essential_words: [], input=None, output:TextIO = sys.stdout):
+    def __init__(self, essential_phrases: [], input=None, output:TextIO = sys.stdout):
         super().__init__(input, output)
-        self.essential_words = essential_words
+        self.essential_phrases = essential_phrases
 
     def emitToken(self, t):
         super().emitToken(t)
         if t.type == JavaLexer.STRING_LITERAL:
-            self.essential_words.append(t.text)
+            self.essential_phrases.append(t.text)
 
 
 def extract_essential_phrases_from_java(source_code: str) -> [str]:
