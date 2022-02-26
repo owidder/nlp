f9b388ac8bb6337dd96fea22697b69581fd5d9f3
Oliver Widder <oliver.widder@gmail.com>
2022-02-26 12:16:38 +0100

    no strings

--- python/antlr/extract_essential_phrases_from_java_with_strings.py
+++ python/antlr/extract_essential_phrases_from_java_with_strings.py
@@ -1,4 +1,6 @@
 from antlr4 import CommonTokenStream, InputStream, ParseTreeWalker
+from typing import TextIO
+import sys
 
 
 from python.antlr.java.JavaLexer import JavaLexer
@@ -20,9 +22,21 @@ class JavaListener(JavaParserListener):
         self.essential_phrases.append(ctx.getChild(1).getText())
 
 
+class StringJavaLexer(JavaLexer):
+    def __init__(self, essential_phrases: [], input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.essential_phrases = essential_phrases
+
+    def emitToken(self, t):
+        super().emitToken(t)
+        if t.type == JavaLexer.STRING_LITERAL:
+            self.essential_phrases.append(t.text)
+
+
 def extract_essential_phrases_from_java(source_code: str) -> [str]:
     essential_phrases = []
-    parser = JavaParser(CommonTokenStream(JavaLexer(InputStream(source_code))))
+    lexer = StringJavaLexer(essential_phrases, InputStream(source_code))
+    parser = JavaParser(CommonTokenStream(lexer))
     ParseTreeWalker().walk(JavaListener(essential_phrases), parser.compilationUnit())
 
     return essential_phrases
--- python/antlr/extract_essential_phrases_from_python_with_strings.py
+++ python/antlr/extract_essential_phrases_from_python_with_strings.py
@@ -1,4 +1,6 @@
 from antlr4 import CommonTokenStream, InputStream, ParseTreeWalker
+import sys
+from typing import TextIO
 
 
 from python.antlr.python.Python3Parser import Python3Parser
@@ -17,9 +19,22 @@ class PythonListener(Python3Listener):
         self.essential_phrases.append(ctx.getChild(1).getText())
 
 
+class StringPythonLexer(Python3Lexer):
+
+    def __init__(self, essential_phrases: [], input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.essential_phrases = essential_phrases
+
+    def emitToken(self, token):
+        super().emitToken(token)
+        if token.type == Python3Lexer.STRING or token.type == Python3Lexer.STRING_LITERAL:
+            self.essential_phrases.append(token.text)
+
+
 def extract_essential_phrases_from_python(source_code: str) -> [str]:
     essential_phrases = []
-    parser = Python3Parser(CommonTokenStream(Python3Lexer(InputStream(source_code))))
+    lexer = StringPythonLexer(essential_phrases, InputStream(source_code))
+    parser = Python3Parser(CommonTokenStream(lexer))
     ParseTreeWalker().walk(PythonListener(essential_phrases), parser.file_input())
 
     return essential_phrases
