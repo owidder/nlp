c85fca262695784bfb7935a50d5072a9c79bbe80
Oliver Widder <oliver.widder@gmail.com>
2022-03-27 11:38:37 +0200

    start all

--- start-vector-do-all.sh
+++ start-vector-do-all.sh
@@ -1,2 +1,7 @@
 ./start-vector-do-gen.sh school
 ./start-vector-do-gen.sh metasfresh
+./start-vector-do-gen.sh adempiere
+./start-vector-do-gen.sh axelor-open-suite
+./start-vector-do-gen.sh erpnext
+./start-vector-do-gen.sh odoo
+./start-vector-do-gen.sh ofbiz-framework
