ff32c795879cbcb4a98d1eceeb35fcc5afaf2ce4
Oliver Widder <oliver.widder@gmail.com>
2022-03-27 11:02:06 +0200

    output to _all

--- start-vector-do-gen.sh
+++ start-vector-do-gen.sh
@@ -13,4 +13,4 @@ export MIN_TFIDF=0
 export PATH_TO_JAR=/root/dev/antlr/build/libs/antlr-1.0-SNAPSHOT.jar
 export USE_ANTLR=True
 export EXCLUDE_FOLDERS=static,lib,jquery
-python3 -m python.create_vectors --outpath /root/dev/nlp/dict/$1 --docpath /root/dev/erp_doc/$1
+python3 -m python.create_vectors --outpath /root/dev/nlp/dict/_all/$1 --docpath /root/dev/erp_doc/$1
