7c216ddb99f4989d4e10a4a9d06cced074455d18
Oliver Widder <oliver.widder@gmail.com>
2022-05-11 16:36:58 +0200

    remove call_server.py

--- python/call_server.py
+++
@@ -1,6 +0,0 @@
-from py4j.java_gateway import JavaGateway
-
-gateway = JavaGateway()
-
-result = gateway.entry_point.startListener("/Users/oliverwidder/Documents/dev/erp_doc/erpnext/code/restaurant/doctype/restaurant/restaurant.py")
-print(result)
